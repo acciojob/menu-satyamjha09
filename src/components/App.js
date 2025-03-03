6c2852a275382c3b0f04ebc6477f9eb933a0e652
satyamjha <satyamjhaactor07@gmail.com>
2025-03-03 19:27:03 +0530

    Done Projects

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,38 @@
 import React, { useState } from 'react'
-import menuItems from './data'
-import Menu from './Menu';
+import Menu from './Menu'
+import Categories from './Categories';
+import menu from "./data";
 import "../styles/App.css"
+
+ const allCategories = ["All" , ...new Set(menu.map((item) => item.category))];
+
 
 const App = () => {
 
-  const [items, setItems] = useState(menuItems);
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if(category === 'all') {
-      setItems(menuItems);
-    } else {
-      setItems(menuItems.filter((item) => item.category === category))
+    if (category === "All") {
+      setMenuItems(menu);
+      return;
     }
-  }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
-    <div className="container">
-       <h1>Our Menu</h1>
-       <div className="buttons">
-       <button onClick={() => filterItems('all')}>All</button>
-        <button onClick={() => filterItems('breakfast')}>Breakfast</button>
-        <button onClick={() => filterItems('lunch')}>Lunch</button>
-        <button onClick={() => filterItems('shakes')}>Shakes</button>
-       </div>
-       <Menu items={items} />
-    </div>
+    <main>
+      <section>
+        <div className="title">
+          <h2> Our Menu </h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+        
+      </section>
+    </main>
   )
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,20 +2,22 @@ import React from 'react'
 
 const Menu = ({ items }) => {
   return (
-    <div className="menu-list">
+    <div className="section-center">
         {items.map((menuItem) => {
-            const {id , title , category , price , img , desc} = menuItem;
+            const { id, title , img , desc , price } = menuItem;
 
             return (
-                <div key={id} className="menu-item">
-                <img src={img} alt={title} />
-                <div className="info">
-                <h4>{title}</h4>
-                <h5>${price}</h5>
-                <p>{desc}</p>
-                </div>
-                </div>
-            );
+                <article key={id} className="menu-item">
+                    <img src={img} alt={title} className="photo" />
+                    <div className="item-info">
+                    <header>
+                        <h4>{title}</h4>
+                        <h4 className="price">${price}</h4>
+                    </header>
+                    <p className="item-text">{desc}</p>
+                    </div>
+                </article>
+            )
         })}
 
     </div>
